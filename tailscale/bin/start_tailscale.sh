d99bfbf375888e5f5bfaa10da8ee330fc2ef419c
rsrax <rs.wolviepool1499@gmail.com>
2026-02-25 10:50:23 +0000

    Enable Tailscale SSH on device startup

--- tailscale/bin/start_tailscale.sh
+++ tailscale/bin/start_tailscale.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/mnt/us/extensions/tailscale/bin/tailscale up --auth-key=$(cat /mnt/us/extensions/tailscale/bin/auth.key)> tailscale_start_log.txt 2>&1
+/mnt/us/extensions/tailscale/bin/tailscale up --ssh --auth-key=$(cat /mnt/us/extensions/tailscale/bin/auth.key)> tailscale_start_log.txt 2>&1
 
